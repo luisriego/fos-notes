f8a2b8cab0a25ae34e6e471dc48c57654ab50e07
luisriego <luisriego@hotmail.com>
2022-08-03 06:31:13 -0300

    update to sever

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 yarn create @eslint/config
 
-"rules": {
-"react/react-in-jsx-scope": "off"
-}
+    "rules": {
+        "react/react-in-jsx-scope": "off",
+        "react/prop-types": "off"
+    }
--- src/App.js
+++ src/App.js
@@ -1,21 +1,78 @@
 import { useEffect, useState } from 'react'
 import Note from './components/Note'
+import Notification from './components/Notification'
 import FormNote from './components/formNote'
-import notesServices from './services/data'
+import notesService from './services/data'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
 
   useEffect(() => {
-    notesServices.getNotes().then(initialsNotes => {
-      setNotes(initialsNotes)
-    })
+    notesServices
+      .getNotes()
+      .then(initialsNotes => {
+        setNotes(initialsNotes)
+      })
   }, [])
+
+  const toggleImportanceOf = id => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+
+    notesService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(() => {
+        setErrorMessage(
+          `Note '${note.content}' was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+  }
+
+  // const notesToShow = showAll
+  //   ? notes
+  //   : notes.filter(note => note.important)
+
+  const handleLogin = (event) => {
+    event.preventDefault()
+    console.log('logging in with', username, password)
+  }
 
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
       <ul>
         {notes.map(note =>
           <Note key={note.id} note={note} />
